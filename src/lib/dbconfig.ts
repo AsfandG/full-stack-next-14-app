03895884f51af6c8007a2e11ccc51cc04ad478f8
Asfandyar <asfandm17@gmail.com>
2024-04-13 10:14:03 +0500

    first commit

---
+++ src/lib/dbconfig.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+type connectionObject = {
+  isConnected?: number;
+};
+
+const connection: connectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already connected to database");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.mongodb_uri || "");
+    connection.isConnected = db.connections[0].readyState;
+    console.log("DB connected!");
+  } catch (error) {
+    console.log("db connection failed!");
+    process.exit(1);
+  }
+}
+
+export default dbConnect;
